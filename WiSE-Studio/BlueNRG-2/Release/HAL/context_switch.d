a5a673cc9d19776bb909dcf248b21d6a1cf511a6
jacobspiller <57590860+jacobspiller@users.noreply.github.com>
2023-03-20 13:05:34 -0600

    Delete WiSE-Studio directory

--- WiSE-Studio/BlueNRG-2/Release/HAL/context_switch.d
+++
@@ -1,5 +0,0 @@
-HAL/context_switch.o: \
- C:/Users/jspil/ST/BlueNRG-1_2\ DK\ 3.2.3/Library/hal/src/context_switch.s \
- c:\users\jspil\st\bluenrg-1_2\ dk\ 3.2.3\library\hal\inc\asm.h
-
-c:\users\jspil\st\bluenrg-1_2\ dk\ 3.2.3\library\hal\inc\asm.h:
